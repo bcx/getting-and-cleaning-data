b66f440cf9c26d2420a79bf8fca3a3c2e573b47d
bcx <bill.curtis@gmail.com>
2015-06-21 15:20:22 -0700

    fix typo

--- run_analysis.R
+++ run_analysis.R
@@ -7,9 +7,9 @@
 features <- read.table("features.txt")
 
 # Feature names are in the 2nd column of the data we just read.
-features <- features$V2  
+features <- features$V2
 
-# Read in the lookup date for the activies.  This lookup table
+# Read in the lookup data for the activies.  This lookup table
 # will be used to replace the activity codes in the y_ files
 # with descriptive names.
 labels <- read.table("activity_labels.txt")
@@ -24,23 +24,23 @@ names(labels) <- c("activity_code", "activity")
 # The 7352 observations of 561 variables.
 X_train <- read.table("train/X_train.txt")
 
-# The activity codes for out 7352 observations.
+# The activity codes for our 7352 observations.
 y_train <- read.table("train/y_train.txt")
 
-# The subject numbers (IDs) for out 7352 observations.
+# The subject numbers (IDs) for our 7352 observations.
 subject_train <- read.table("train/subject_train.txt")
 
 # =====================================
 # Read in the test data set.
 # =====================================
 
-# The 2947 observations of 561 variables.
-X_test <- read.table("test/X_test.txt")   
+# The 2947 observations of our 561 variables.
+X_test <- read.table("test/X_test.txt")
 
-# The activity codes for out 2947 observations.
+# The activity codes for our 2947 observations.
 y_test <- read.table("test/y_test.txt")
 
-# The subject numbers (IDs) for out 2947 observations.
+# The subject numbers (IDs) for our 2947 observations.
 subject_test <- read.table("test/subject_test.txt")
 
 # =======================================
@@ -57,7 +57,7 @@ subject_data <- rbind(subject_train, subject_test)
 # Label the combined observations with the feature names.
 colnames(X_data) <- features
 
-# Label the subject columns and activity_codes.
+# Label the subject and activity_code columns.
 colnames(subject_data) <- "subject"
 colnames(y_data) <- "activity_code"
 
@@ -74,15 +74,15 @@ activity <- as.character(labels[match(y_data$activity_code, labels$activity_code
 selected_data <- X_data[,grepl("mean\\(\\)|std\\(\\)", colnames(X_data))]
 
 # =======================================
-# Combine the subject IDs, the activity names, and the 
-# mean & std observations together.  This is our first 
+# Combine the subject IDs, the activity names, and the
+# mean & std observations together.  This is our first
 # "tidy" data set, used to create the uploaded .txt file.
 # =======================================
 data <- cbind(subject_data, activity, selected_data)
 
 # =======================================
-# For each (subject, activity) pair, compute the mean of
-# each observation.  This is out second tidy data set.
+# For each subject-activity pair, compute the mean of
+# each observation.  This is our second tidy data set.
 # =======================================
 means_by_subject_and_activity = aggregate( .~ subject + activity, data=data, FUN=mean )
 
@@ -90,6 +90,3 @@ means_by_subject_and_activity = aggregate( .~ subject + activity, data=data, FUN
 # Output the final data set.
 # =======================================
 print(means_by_subject_and_activity)
-
-
-
